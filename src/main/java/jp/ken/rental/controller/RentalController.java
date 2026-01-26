fc8cb820df60403069d4d0d0a26d3f30998145c9
keroppi <kero2return@gmail.com>
2026-01-26 13:26:55 +0900

    Controllerテスト変更

--- src/main/java/jp/ken/rental/controller/RentalController.java
+++ src/main/java/jp/ken/rental/controller/RentalController.java
@@ -1,8 +1,13 @@
 package jp.ken.rental.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RentalController {
 
+	@GetMapping("test")
+	public String test() {
+		return "test";
+	}
 }
