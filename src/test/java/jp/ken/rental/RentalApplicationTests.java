6d90fe11eb48deb577e596e96d0d8e8192188fe2
yoshimura <ichiyasa-g-3@example.com>
2026-01-26 12:48:12 +0900

    初期設定

---
+++ src/test/java/jp/ken/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ken.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
