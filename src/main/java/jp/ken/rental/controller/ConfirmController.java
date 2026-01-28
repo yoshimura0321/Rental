85bf2fc78844849d0b949635f7301524a4a742c1
ehara <algkm1007@gmail.com>
2026-01-28 13:06:01 +0900

    コントローラーの編集

--- src/main/java/jp/ken/rental/controller/ConfirmController.java
+++ src/main/java/jp/ken/rental/controller/ConfirmController.java
@@ -2,14 +2,24 @@ package jp.ken.rental.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import jp.ken.rental.application.service.UserInsertService;
+import jp.ken.rental.form.UserForm;
+
 @Controller
 @RequestMapping("/confirm")
-@SessionAttributes({"UserForm"})
+@SessionAttributes({"userForm"})
 public class ConfirmController {
+	
+	private UserInsertService userInsertService;
+		
+		public ConfirmController(UserInsertService userInsertService) {
+			this.userInsertService = userInsertService; 
+		}
 	
 	@GetMapping
 	public String toConfirm() {
@@ -20,7 +30,12 @@ public class ConfirmController {
 		return "redirect:/regist";
 	}
 	@PostMapping(params = "forward")
-	public String forFinish() {
+	public String forFinish(@ModelAttribute UserForm userForm) throws Exception{
+		int row = userInsertService.registUser(userForm);
+		
+		if(row == 0) {
+			return "userRegist";
+		}
 		return "forward:/finish";
 	}
 
