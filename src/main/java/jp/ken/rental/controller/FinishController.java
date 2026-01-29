7b9ba945324f0421a1ef52c4cf8d04ae3003801b
yoshimura <ichiyasa-g-3@example.com>
2026-01-29 15:41:51 +0900

    お願いします

--- src/main/java/jp/ken/rental/controller/FinishController.java
+++ src/main/java/jp/ken/rental/controller/FinishController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
-@RequestMapping("/userFinish")
+@RequestMapping("/finish")
 @SessionAttributes({"userForm"})
 public class FinishController {
 	
