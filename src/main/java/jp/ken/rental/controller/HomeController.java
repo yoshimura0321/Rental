25600676f47e8b1cf51efec80183a9e3076fb6a3
yoshimura <ichiyasa-g-3@example.com>
2026-01-29 15:14:11 +0900

    homecontroller追加

---
+++ src/main/java/jp/ken/rental/controller/HomeController.java
@@ -0,0 +1,18 @@
+/*
+package jp.ken.rental.controller;
+
+import org.springframework.stereotype.Controller;
+
+import jp.ken.rental.application.service.ProductSearchService;
+@Controller
+public class HomeController {
+	private ProductSearchService productSearchService;
+	
+	public HomeController(ProductSearchService productSearchService) {
+		
+		this.productSearchService = productSearchService;
+	}
+
+	@GetMapping
+}
+*/
