f95c91cf1e6078c2166b801947a5860530eea43d
ehara <algkm1007@gmail.com>
2026-01-30 13:16:01 +0900

    securityconfigurationクラスを追加

---
+++ src/main/java/jp/ken/rental/common/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package jp.ken.rental.common.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+	
+	private DataSource dataSource;
+	
+	public SecurityConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	@Bean
+	protected SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/home").permitAll()
+				.requestMatchers("/regist").permitAll()
+				.requestMatchers("/cart").authenticated()
+				.requestMatchers("/css/**").permitAll()
+				.anyRequest().authenticated());
+		
+		http.formLogin(login -> login
+				.defaultSuccessUrl("/home")
+				.permitAll());
+		
+		
+		return http.build();
+	}
+	
+	@Bean
+	protected UserDetailsManager userDetailsManager() {
+		JdbcUserDetailsManager users = new JdbcUserDetailsManager(this.dataSource);
+		return users;
+	}
+}
