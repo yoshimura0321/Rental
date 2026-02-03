08a623635e30ad83cdfc115a0db4762cc42368a4
ehara <algkm1007@gmail.com>
2026-02-03 11:40:54 +0900

    homecontrollerの改悪

--- src/main/java/jp/ken/rental/controller/HomeController.java
+++ src/main/java/jp/ken/rental/controller/HomeController.java
@@ -1,11 +1,11 @@
 package jp.ken.rental.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.ken.rental.application.service.ProductSearchService;
@@ -20,7 +20,30 @@ public class HomeController {
     public HomeController(ProductSearchService productSearchService) {
         this.productSearchService = productSearchService;
     }
+    
+    @GetMapping("/home")
+    public String home(@RequestParam Optional<String> productName, Model model) throws Exception {
+    	
+    	if(productName == null || productName.toString().isBlank()) {
+    		List<ProductForm> productList = productSearchService.getLatest5Products();
+            model.addAttribute("productList", productList);
 
+            return "home";
+    	}else {
+    		// ProductForm にセット
+            ProductForm form = new ProductForm();
+            form.setProductName(productName.toString());
+
+            // Service に渡す
+            List<ProductForm> productList = productSearchService.getProductList(form);
+
+            model.addAttribute("productList", productList);
+            model.addAttribute("headline", "検索結果");
+
+            return "home";
+    	}
+    }
+/*
     // ★ GET：最新5件を表示
     @GetMapping("/home")
     public String home(Model model) throws Exception {
@@ -46,5 +69,5 @@ public class HomeController {
         model.addAttribute("headline", "検索結果");
 
         return "home";
-    }
+    }*/
 }
