efe0bae2a400057f548d4e75803b85ae4d541d96
yoshimura <ichiyasa-g-3@example.com>
2026-02-03 13:58:00 +0900

    MypageControllerの修正

--- src/main/java/jp/ken/rental/controller/MypageController.java
+++ src/main/java/jp/ken/rental/controller/MypageController.java
@@ -12,53 +12,43 @@ import jp.ken.rental.form.UserEntity;
 
 @Controller
 public class MypageController {
-	
-	private UserUpdateService userUpdateService;
-	private UserDeleteService userDeleteService;
 
-	public MypageController(UserUpdateService userUpdateService, 
-							UserDeleteService userDeleteService) {
-		this.userUpdateService = userUpdateService;
-		this.userDeleteService = userDeleteService;	
-	}
-	
-	//ログインしてないとマイページに行けないようにしています。
-	//多分loginControllerのPostでセッション管理する必要がある？
-	public String mypage(HttpSession session, Model model) {
-		
-		UserEntity loginUser = (UserEntity)
-		session.getAttribute("loginUser");
-			if (loginUser == null) {
-				return "redirect:/login";
-			}
-		model.addAttribute("user", loginUser);
-		return "mypage"; 
-	}
-	@GetMapping("/mypage")
-	public String updateForm(HttpSession session, Model model) {
-		UserEntity loginUser = (UserEntity) session.getAttribute("loginUser");
-		if (loginUser == null){
-			return "redirect:/login";
-		}
-		model.addAttribute("userForm", loginUser);
-		
-		return "mypage";
-		}
-	
-	@PostMapping("/profileConfirm") 
-	public String updateConfirm(UserEntity form, Model model) {
-		
-		model.addAttribute("userForm", form);
-		
-		return "mypage/profileConfirm";
-	}
-	
-	@PostMapping("/profileFinish")
-	public String updateComplete(UserEntity form, HttpSession session) throws Exception {
-		
-		userUpdateService.updateUser(form);
-		session.setAttribute("loginUser", form);
-		
-		return "mypage/profileFinish";
-	}
+    private final UserUpdateService userUpdateService;
+    private final UserDeleteService userDeleteService;
+
+    public MypageController(UserUpdateService userUpdateService,
+                            UserDeleteService userDeleteService) {
+        this.userUpdateService = userUpdateService;
+        this.userDeleteService = userDeleteService;
+    }
+
+    @GetMapping("/mypage")
+    public String mypage(HttpSession session, Model model) {
+
+        UserEntity loginUser = (UserEntity) session.getAttribute("loginUser");
+
+        if (loginUser == null) {
+            return "redirect:/login";
+        }
+
+        model.addAttribute("userForm", loginUser);
+        return "mypage/mypage";
+    }
+
+    @PostMapping("/profileConfirm")
+    public String updateConfirm(UserEntity form, Model model) {
+
+        model.addAttribute("userForm", form);
+        return "mypage/profileConfirm";
+    }
+
+    @PostMapping("/profileFinish")
+    public String updateComplete(UserEntity form, HttpSession session) throws Exception {
+
+        userUpdateService.updateUser(form);
+        session.setAttribute("loginUser", form);
+
+        return "mypage/profileFinish";
+    }
+    //デリートはまだだよ～
 }
